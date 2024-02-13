cb57a7caab11b12b745ca5ac8ae6c9fad9d1f934
asheikm <MohamedAsheik.A@BRILLIO.COM>
2024-02-13 19:17:20 +0530

    RBAC changes

--- handlers/user.go
+++ handlers/user.go
@@ -3,6 +3,9 @@ package handlers
 import (
 	"net/http"
 
+	"fmt"
+	"io"
+	"um/models"
 	"um/repositories"
 
 	"github.com/gin-gonic/gin"
@@ -19,13 +22,29 @@ func NewUserHandler(userRepository repositories.UserRepository) *UserHandler {
 }
 
 func (h *UserHandler) Signup(c *gin.Context) {
-	// Access UserRepository methods here
-	// For example:
-	// h.UserRepository.CreateUser(...)
-	// h.UserRepository.GetUserByID(...)
-	// h.UserRepository.UpdateUser(...)
-	// h.UserRepository.DeleteUser(...)
-	c.JSON(http.StatusOK, gin.H{"message": "Sign up successful"})
+	var user models.User
+
+	body, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		// Handle error reading body
+		return
+	}
+
+	fmt.Println("Received JSON data:", string(body))
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON data"})
+		return
+	}
+
+	// TODO: Validate user data if needed (e.g., check for empty fields)
+
+	err = h.UserRepository.CreateUser(&user)
+	if err != nil {
+		// Handle error
+		return
+	}
+	c.JSON(http.StatusCreated, gin.H{"message": "User created successfully"})
 }
 
 func (h *UserHandler) Login(c *gin.Context) {
--- models/user.go
+++ models/user.go
@@ -7,7 +7,7 @@ type User struct {
 	ID        int64     `json:"id"`
 	Username  string    `json:"username" validate:"required,min=3,max=32"`
 	Email     string    `json:"email" validate:"required,email"`
-	Password  string    `json:"password" validate:"required,min=8"` // Consider hashed password storage
+	Password  string    `json:"password" validate:"required,min=8"`
 	IsActive  bool      `json:"isActive"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
@@ -17,9 +17,9 @@ type User struct {
 	FirstName            string   `json:"firstName"`
 	LastName             string   `json:"lastName"`
 	Organization         string   `json:"organization"`
-	Experience           string   `json:"experience"`       // Years of experience
-	AreasOfExpertise     []string `json:"areasOfExpertise"` // e.g., Web App Pen Testing, Mobile App Pen Testing
-	Certifications       []string `json:"certifications"`   // e.g., OSCP, CEH, etc.
+	Experience           string   `json:"experience"`
+	AreasOfExpertise     []string `json:"areasOfExpertise"`
+	Certifications       []string `json:"certifications"`
 	TwoFactorAuthEnabled bool     `json:"twoFactorAuthEnabled"`
 
 	// Project specfic
